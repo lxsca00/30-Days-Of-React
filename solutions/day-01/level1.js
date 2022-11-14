716b582956353e2e559fe040fa72362441160d13
lxsca00 <ljimena10@gmail.com>
2022-11-13 23:33:15 -0500

    something in day-01

---
+++ solutions/day-01/level1.js
@@ -0,0 +1,28 @@
+console.log("Refreshing JS!");
+
+const arr = []; // Recomended way to create a empty array
+
+let js = "Javascript";
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+webTechs[1] = "Durazno";
+
+Array(8).fill("A");
+
+const firstList = [1, 2, 3, 4];
+const secondList = [5, 6, 7, 8];
+
+let inde = firstList.indexOf(2);
+
+
+
+inde != -1 ? console.log("wtf is dis") : console.log("A");
